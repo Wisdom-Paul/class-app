c4d8bfbf387f82b158deb13ac0a446d05ae6c917
Wisdom-Paul <159251775+Wisdom-Paul@users.noreply.github.com>
2024-02-16 14:18:05 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { Text, View, Image, Button, TouchableOpacity } from "react-native";
+//import Duolingo from "./components/duolingo";
+import LoationPermit from "./components/LoationPermit";
+//import DeliveryMan from "./components/DeliveryMan";
+import { NavigationContainer } from "@react-navigation/native";
+
+const App = () => {
+  return(
+  
+    <LoationPermit/>
+
+  )
+};
+export default App;
